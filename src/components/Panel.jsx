673d412d311d7cc92155a791d207f1c633a22925
meteora277 <justin.s.diaz@gmail.com>
2022-01-27 16:19:49 -0500

    attach onSelect to Panel onClick

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 export default class Panel extends Component {
   render() {
-    const {label, value} = this.props
+    const {label, value, onSelect} = this.props
 
     return (
-      <section>
+      <section onClick={onSelect}>
         <h1 className='dashboard__panel-header'>{label}</h1>
         <p className='dashboard__panel-value'>{value}</p>
       </section>
