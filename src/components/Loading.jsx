490cabe44a754b9a3e47dc3316e21aa33b92bf3b
meteora277 <justin.s.diaz@gmail.com>
2022-01-27 15:22:17 -0500

    formating

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-    import React, { Component } from 'react';
-=======
 import React, { Component } from 'react';
->>>>>>> 7b32a3e1d1eda47a3334e0b5234a272d2339af9a
 
 export default class Loading extends Component {
   render() {
